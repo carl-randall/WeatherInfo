a19802f26299614a7c6c3d66299e820895c24e0e
Carl Randall <hey@carl-randall.net>
2024-09-26 17:48:04 +0100

    Update UK.cs

--- UK.cs
+++ UK.cs
@@ -3,10 +3,11 @@
     public class UnitedKingdom
     {
         public int Identifier { get; set; }       
-        public string {get; set;}
+        public string Label {get; set;}
+        
     }
 
-    public record Coordinates(int Lat, int Lon);
+     public record Coordinates(int Lat, int Lon);  
 
      
 }
